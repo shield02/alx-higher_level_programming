83c2a2cd338abe3a6991cdca5ce107889df9514a
Otuekong <myshield02@gmail.com>
2023-05-09 20:20:28 +0100

    modified code to print correct output with correct type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ elif lastd == 0:
 elif lastd > 5:
     print(f'Last digit of {number:d} is {lastd:d} and is greater than 5')
 elif lastd < 6 and lastd != 0:
-    print(f'Last digit of {number:d} is {lastd:d} {less6}')
+    print(f'Last digit of {number:d} is {lastd:d} '+less6)
