f5ae87ee9f864d54aa10c9009e9d97b634c859a8
Otuekong <myshield02@gmail.com>
2023-05-09 15:58:33 +0100

    modified code to cover less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0 and int(str(number)[-1]) != 0:
-    print(f'Last digit of {number:d} is {str(number)[0]}{int(str(number)[-1]):d} and is less than 6 and not 0')
+    print(f'Last digit of {number:d} is {str(number)[0]}{int(str(number)[-1]):d} \
+          and is less than 6 and not 0')
 elif number < 0 and int(str(number)[-1]) == 0:
-    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} and is less than 6 and not 0')
+    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} \
+          and is less than 6 and not 0')
 elif int(str(number)[-1]) == 0:
-    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} and is 0')
+    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} \
+          and is 0')
 elif int(str(number)[-1]) > 5:
-    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} and is greater than 5')
+    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} \
+          and is greater than 5')
 elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
-    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} and is less than 6 and not 0')
+    print(f'Last digit of {number:d} is {int(str(number)[-1]):d} \
+          and is less than 6 and not 0')
