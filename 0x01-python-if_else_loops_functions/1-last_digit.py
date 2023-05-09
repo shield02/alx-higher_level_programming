11f59c9b696c9d6f8cbf0cdfa563e24ba137de65
Otuekong <myshield02@gmail.com>
2023-05-09 20:11:37 +0100

    modified code to print correct output with correct type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 lastd = int(str(number)[-1])
 less6 = 'and is less than 6 and not 0'
 if number < 0 and lastd != 0:
-    print(f'Last digit of {number:d} is {str(number)[0]}{lastd:d} {less6}')
+    print(f'Last digit of {number:d} is -{lastd:d} {less6}')
 elif number < 0 and lastd == 0:
     print(f'Last digit of {number:d} is {lastd:d} and is 0')
 elif lastd == 0:
