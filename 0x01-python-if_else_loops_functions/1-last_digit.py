1ac04b7a108300aa545dfc46f6529ddce9275565
Otuekong <myshield02@gmail.com>
2023-05-09 15:53:12 +0100

    modified code to cover less than 0wq

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 lastd = int(str(number)[-1])
-if lastd == 0:
+if number < 0 and lastd != 0:
+    print(f'Last digit of {number:d} is {str(number)[0]}{lastd:d} and is less than 6 and not 0')
+elif number < 0 and lastd == 0:
+    print(f'Last digit of {number:d} is {lastd:d} and is less than 6 and not 0')
+elif lastd == 0:
     print(f'Last digit of {number:d} is {lastd:d} and is 0')
 elif lastd > 5:
     print(f'Last digit of {number:d} is {lastd:d} and is greater than 5')
