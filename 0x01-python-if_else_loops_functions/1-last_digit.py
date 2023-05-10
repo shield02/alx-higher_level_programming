0e0d24e0cd54954067cc3405705165c13add435a
Otuekong <myshield02@gmail.com>
2023-05-10 22:08:59 +0100

    code to print with correct type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastd = int(str(number)[-1])
+lastd = number%10
 less6 = 'and is less than 6 and not 0'
 if number < 0 and lastd != 0:
-    print(f'Last digit of {number:d} is {str(number)[0]}{lastd:d} '+less6)
+    print(f'Last digit of {number:d} is {number%-10:d} '+less6)
 elif number < 0 and lastd == 0:
-    print(f'Last digit of {number:d} is {int(lastd):d} and is 0')
+    print(f'Last digit of {number:d} is {lastd:d} and is 0')
 elif lastd == 0:
-    print(f'Last digit of {number:d} is {int(lastd):d} and is 0')
+    print(f'Last digit of {number:d} is {lastd:d} and is 0')
 elif lastd > 5:
     print(f'Last digit of {number:d} is {lastd:d} and is greater than 5')
 elif lastd < 6 and lastd != 0:
