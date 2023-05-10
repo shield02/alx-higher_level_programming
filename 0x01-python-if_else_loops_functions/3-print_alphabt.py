7e8d594d28fe7e4de7601e26d1662f423121b870
Otuekong <myshield02@gmail.com>
2023-05-10 18:45:27 +0100

    code to print ascii alphabets not q and e in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z')+1):
     if (char != ord('e') and char != ord('q')):
-        print('{}'.format(chr(char)), end="")
+        print("{:s}".format(chr(char)), end="")
