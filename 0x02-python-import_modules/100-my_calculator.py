6f04ed3a1e26ac6757afa9e8d53828c3b4521525
Otuekong <myshield02@gmail.com>
2023-05-12 04:22:14 +0100

    code to import all functions from a file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    args = sys.argv
+    argslen = len(args)
+    if argslen != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operators = args[2]
+    a = int(args[1])
+    b = int(args[3])
+
+    def get_operator(argument):
+        sign = {
+            "+": add(a, b),
+            "-": sub(a, b),
+            "*": mul(a, b),
+            "/": div(a, b)
+        }
+        return (sign.get(argument))
+    if args[2] in ('+', '-', '*', '/'):
+        print("{} {} {} = {}".format(a, operators, b, get_operator(operators)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
