f98af760c97756cfd1e7092a2a37810828b8f8a9
Otuekong <myshield02@gmail.com>
2023-05-12 03:57:58 +0100

    code to print the result of adding all args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argslen = len(sys.argv) - 1
-    k = 0
+    output = 0
     for i in range(1, argslen + 1):
-        k += int(sys.argv[i])
-    print(k)
+        output += int(sys.argv[i])
+    print(output)
