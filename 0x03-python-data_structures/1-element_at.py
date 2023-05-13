b920e249f1f2969251025bb6ebec9679537aa7b5
Otuekong <myshield02@gmail.com>
2023-05-13 13:42:01 +0100

    code to print element of a list at a given index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    list_len = len(my_list)
+
+    while True:
+        if(idx < 0):
+            return None
+        elif(idx > list_len):
+            return None
+        else:
+            return my_list[idx]
