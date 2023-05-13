a05084ce76a98ebb8e5ade4a42717258d9146d90
Otuekong <myshield02@gmail.com>
2023-05-13 13:47:30 +0100

    code to ensure it returns in correct format

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    list_len = len(my_list)
 
-    while True:
-        if(idx < 0):
-            return None
-        elif(idx > list_len):
-            return None
-        else:
-            return int(my_list[idx])
+    if(idx < 0):
+        return None
+    elif(idx > len(my_list)):
+        return None
+    else:
+        return int(my_list[idx])
