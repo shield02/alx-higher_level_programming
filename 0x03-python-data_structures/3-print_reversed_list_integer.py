44486d1e3be738eb5ce900cb2650e90247c32f0e
Otuekong <myshield02@gmail.com>
2023-05-13 21:10:51 +0100

    code to print list element in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list)
     if my_list is None or my_list == []:
-        return
-    for i in range(1, n+1):
-        print('{:d}'.format(my_list[-i]))
+        return None
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
