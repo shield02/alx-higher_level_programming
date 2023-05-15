3a0946e204a4ebdddf4c9f5aa00e5465029991bd
Otuekong <myshield02@gmail.com>
2023-05-15 21:46:46 +0100

    code to print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for left in range(len(matrix)):
+        for right in range(len(matrix[left])):
+            print("{:d}".format(matrix[left][right]), end="")
+            if (right != len(matrix[left]) - 1):
+                print(" ", end="")
+        print("$")
