cdcd6ab6a7d94ea774de496495aae456cc440304
Otuekong <myshield02@gmail.com>
2023-05-17 23:30:45 +0100

    code to delete key for a specific value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary.values():
+    if (value not in a_dictionary.values()):
+        return a_dictionary
+    if (a_dictionary is None) or (len(a_dictionary) == 0):
         return a_dictionary
     
     for key in a_dictionary.copy():
