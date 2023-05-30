b805efd375c08586528fce47d48ed0f22f3601bd
Otuekong <myshield02@gmail.com>
2023-05-30 20:36:59 +0100

    an empty class that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,17 +2,5 @@
 
 # Define an empty class
 class Square:
-    """This is an empty square class that defines nothing
-
-    This is simply an empty class with nothing in it. This
-    is an empty class. This is an empty class. This is an empty
-    class. This is an empty class. 
-
-    Args:
-        This class has no arguments
-
-    Attributes:
-        This class has no attributes
-
-    """
+    """This is an empty square class that defines nothing"""
     pass
