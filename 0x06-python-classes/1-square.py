1e1d556717b2d01153e8e12b1cbb53bcf510918c
Otuekong <myshield02@gmail.com>
2023-05-31 04:12:32 +0100

    code to create a class with a private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Creates an empty class called Square"""
+""" Define an empty class"""
 
 
 class Square:
     """ This is an empty class with size private attribute """
     def __init__(self, size):
         """
-                Instantiation with size
+            Instantiate the class
         Args:
             size: size of the square
         """
