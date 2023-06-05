00d5be4f5023b82e8f2ede8444265cefaa5d501f
shield <myshield02@gmail.com>
2023-06-05 19:43:00 +0100

    code to add width and height class property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,11 +15,6 @@ class Rectangle:
         """ Get the width property"""
         return self.__width
 
-    @property
-    def height(self):
-        """ Get the height property"""
-        return self.__height
-
     @width.setter
     def width(self, value):
         """ Set the width property"""
@@ -28,6 +23,11 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+
+    @property
+    def height(self):
+        """ Get the height property"""
+        return self.__height
 
     @height.setter
     def height(self, value):
