aabef2af0b5c8ceaadcd165113076910b5266f04
shield <myshield02@gmail.com>
2023-06-10 02:51:08 +0100

    code to multiply two matrices

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Multiply two matrix the hard way"""
+
+
+def matrix_mul(m_a, m_b):
+    """This function multiplies two matrices
+    Args:
+        m_a: (list of list) First matrix
+        m_b: (list of list) Second matrix
+    Raises:
+        TypeError: If either m_a or m_b is not a list.
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        ValueError: If either m_a or m_b is empty.
+        TypeError: If m_a and m_b element is not a int or float.
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied
+    Returns:
+        A matrix (list of list)
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise isinstance("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    if not all(isinstance(elem, int) or isinstance(elem, float)
+            for elem in [elem for row in m_a for elem in row]):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(elem, int) or isinstance(elem, float)
+            for elem in [elem for row in m_b for elem in row]):
+        raise TypeError("m_b should contain only integers or floats")
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(row[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+
+    
