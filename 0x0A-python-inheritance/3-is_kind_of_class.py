c9dc451d873341246607ab767184ff63c806adab
shield <myshield02@gmail.com>
2023-06-14 13:42:20 +0100

    check if object is instance of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Check same class or inherited module"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if object is instance of class
+    Args:
+        obj: instance of a class
+        a_class: a class
+    Raises:
+        Nothing
+    Returns:
+        True or False
+    """
+    return isinstance(obj, a_class)
