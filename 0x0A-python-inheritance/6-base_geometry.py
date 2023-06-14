1555ce725eb9aa846c08bfefcbf3bf30bca84c9d
shield <myshield02@gmail.com>
2023-06-14 16:41:08 +0100

    added a public instance method that raises exception only

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Geometry class Module"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        """Raise an exception, not implemented
+        Args:
+            Nothing
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
