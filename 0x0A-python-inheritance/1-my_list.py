0c4f4f54916a8e85ce19a24b2eb382ed7a82ebb7
shield <myshield02@gmail.com>
2023-06-14 10:21:49 +0100

    class that inherits from a list class and print the list sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Inherit from a list"""
+
+
+class MyList(list):
+    """A simple class that inherits another class"""
+    def print_sorted(self):
+        """Prints a sorted list
+        Args:
+            Nothing
+        Raises:
+            Nothing
+        Returns:
+            Sorted list
+        """
+        print(f"{self[:].sort()}")
