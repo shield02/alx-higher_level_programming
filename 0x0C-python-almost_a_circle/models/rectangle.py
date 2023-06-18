29fc3f23f8f8ae514497542a848581576fc3973c
shield <myshield02@gmail.com>
2023-06-18 21:04:09 +0100

    updating the display method taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,7 +75,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print the rectangle instance using # character"""
+        for y in range(self.__y):
+            print()
         for h in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
             for w in range(self.__width):
                 print("#", end="")
             print()
