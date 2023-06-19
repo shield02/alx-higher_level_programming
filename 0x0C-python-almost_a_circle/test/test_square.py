354cedf763f7885da013642451dea4bd9e5c6146
shield <myshield02@gmail.com>
2023-06-19 20:48:12 +0100

    test for attributes and string representation

---
+++ 0x0C-python-almost_a_circle/test/test_square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Unittest cases for the square class"""
+
+
+import io
+import contextlib
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class TestSquare(unittest.TestCase):
+    """Test cases for Square class"""
+
+    def setUp(self):
+        Base.__nb_objects = 0
+
+    def test_attributes(self):
+        """Test attributes"""
+        square0 = Square(1)
+        self.assertEqual(square0.id, 1)
+
+        square1 = Square(5, 3, 4)
+        self.assertEqual(square1.height, 5)
+        self.assertEqual(square1.width, 5)
+        self.assertEqual(square1.x, 3)
+        self.assertEqual(square1.y, 4)
+        self.assertEqual(square1.id, 2)
+    
+    def test_str_rep(self):
+        """Test __str__ representation"""
+        square = Square(9, 4, 5, 6)
+        self.assertEqual(str(square), "[Square] (6) 4/5 - 9")
+
+
+if __name__ == '__main__':
+    unittest.main()
