897b2a3442783a9cff1696f84b6f534e74865b4d
shield <myshield02@gmail.com>
2023-06-19 05:07:06 +0100

    update method that takes variable arguments and update the class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,28 @@ class Rectangle(Base):
     def __str__(self):
         """Print string representation of the rectangle class"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """Assigns arguments to each attribute
+        Args:
+            *args: any number of arguments
+                    1st arg - id
+                    2nd arg - width
+                    3rd arg - height
+                    4th arg - x
+                    5th arg - y
+        """
+        if len(args) != 0 and args is not None:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an int")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+            
