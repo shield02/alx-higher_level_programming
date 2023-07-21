1e3712bccd6df963a894212fe6251fe7e59ec6cc
shield <myshield02@gmail.com>
2023-07-21 06:48:19 +0100

    list all genre by thier rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
--- lists all rows in a database linked to a row in another table
+-- list all genre by thier rating
 SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
