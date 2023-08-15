546ea4c1bcbd9966f3a3a87ef7a28c8b5e10a950
shield <myshield02@gmail.com>
2023-08-15 03:03:17 +0100

    find the second largest num in args

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const ar = process.argv.slice(2).map(Number);
-  const sec_biggest = ar.sort(function (a, b) { return b - a; })[1];
-  console.log(sec_biggest);
+  const sec = ar.sort(function (a, b) { return b - a; })[1];
+  console.log(sec);
 }
