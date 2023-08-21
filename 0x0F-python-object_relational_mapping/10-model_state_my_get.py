c35a9db775befff3d4a0c483c411e620ba60d8aa
shield <myshield02@gmail.com>
2023-08-21 05:37:33 +0100

    add a state object to the database

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""add the State object “Louisiana” to hbtn_0e_6_usa database"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{sys.argv[1]}:\
+                           {sys.argv[2]}@localhost/{sys.argv[3]}',
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
+        print(f"{state.id}")
+    else:
+        print("Not found")
+    session.close()
