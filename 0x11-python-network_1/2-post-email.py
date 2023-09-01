772ef237563672e271533ca90d0f018feedf05bb
shield <myshield02@gmail.com>
2023-09-01 14:12:34 +0100

    take a url and email and send post request

---
+++ 0x11-python-network_1/2-post-email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""take a URL and an email
+Sends a POST request to the URL with the email.
+"""
+import sys
+import urllib
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
