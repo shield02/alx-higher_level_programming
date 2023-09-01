577e5e99dc89a9e97b34c0412e410ffc21069fb3
shield <myshield02@gmail.com>
2023-09-01 13:52:37 +0100

    fetch from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script fetches 'https://alx-intranet.hbtn.io/status' """
+
+import urllib
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print(f'\t- type: {type(html)}')
+    print(f'\t- content: {html}')
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
