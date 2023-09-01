477abb6647c9afd4693e85bebbe8348c6484b279
shield <myshield02@gmail.com>
2023-09-01 03:11:06 +0100

    code to find peak in the list of unordered items

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,8 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
+    if len(list_of_integers) == 0:
+        return None
     if len(list_of_integers) == 1:
         return list_of_integers[:]
     if len(list_of_integers) == 2:
